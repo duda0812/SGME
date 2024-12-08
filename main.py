cfcb9e0248548a8a9850935847fba9efeb8574d8
duda0812 <mariaeduarda.afs08@gmail.com>
2024-12-07 22:43:43 -0400

    teste_branch

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 #           Larissa Nascimento Rodrigues
 #           Maria Eduarda Araújo Frota Saraiva
 
+#trabalho da madu
+
 from material import *
 from usuario import *
 from emprestimo import *
